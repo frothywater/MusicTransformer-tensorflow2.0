5de6d76b7c0d92ebc0df716856ebc97e231cd89a
frothywater <kokoroic@outlook.com>
2021-12-12 07:37:46 +0000

    train: Wikifonia melody with chords

--- params.py
+++ params.py
@@ -1,11 +1,11 @@
 from  processor import pad_word, sos_word, eos_word, dictionary_size
 
-max_seq = 512
+max_seq = 2048
 embedding_dim = 256
 num_layer = 6
-batch_size = 60
+batch_size = 5
 dropout = 0.2
-epochs = 100
+epochs = 200
 
 pad_token = pad_word
 token_sos = sos_word
@@ -13,8 +13,8 @@ token_eos = eos_word
 vocab_size = dictionary_size
 
 gpu_id = "2"
-train_id = "wikifonia-melody-2"
+train_id = "wikifonia-melody-chord"
 model_dir = "data/model"
-load_dir = "data/model"
+load_dir = None
 generated_dir = "data/generated"
 dataset_dir = "data/dataset"
