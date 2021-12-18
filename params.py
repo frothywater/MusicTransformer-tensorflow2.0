9f315b45c72e6d45d61413276a3956e24efe9910
frothywater <kokoroic@outlook.com>
2021-12-18 05:45:43 +0000

    train: Wikifonia v5

--- params.py
+++ params.py
@@ -5,16 +5,18 @@ embedding_dim = 256
 num_layer = 6
 batch_size = 50
 dropout = 0.2
-epochs = 500
+epochs = 200
 
 pad_token = pad_word
 token_sos = sos_word
 token_eos = eos_word
 vocab_size = dictionary_size
 
-gpu_id = "2"
-train_id = "wikifonia-melody-v2"
+gpu_id = "1"
+train_id = "wikifonia-melody-v5"
 model_dir = "data/model"
 load_dir = None
+load_epoch = 5
 generated_dir = "data/generated"
 dataset_dir = "data/dataset"
+words_dir = "data/words"
